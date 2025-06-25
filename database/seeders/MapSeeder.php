5794b30c50f8de653a58f60d35b4c75c8da30232
HO Julien <hojulien02@gmail.com>
2025-06-25 17:40:04 +0200

    Mise à jour du seeder pour Map

--- database/seeders/MapSeeder.php
+++ database/seeders/MapSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Map;
 
 class MapSeeder extends Seeder
 {
@@ -12,6 +13,28 @@ class MapSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $maps = require database_path('data/maps_data.php');
+        foreach ($maps as $map) {
+            Map::create([
+                'rc' => $map['rc'],
+                'artist' => $map['artist'],
+                'title' => $map['title'],
+                'artistUnicode' => $map['artistUnicode'],
+                'titleUnicode' => $map['titleUnicode'],
+                'creator' => $map['creator'],
+                'sr' => $map['sr'],
+                'length' => $map['length'],
+                'cs' => $map['cs'],
+                'hp' => $map['hp'],
+                'ar' => $map['ar'],
+                'od' => $map['od'],
+                'setId' => $map['setId'],
+                'mapId' => $map['mapId'],
+                'submitDate' => $map['submitDate'],
+                'lastUpdated' => $map['lastUpdated'],
+                'tags' => $map['tags'],
+                'background' => $map['background']
+            ]);
+        }
     }
 }
