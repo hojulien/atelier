de59a1a8bc1ba2587b698e9bd137c2294038b8bc
Julien <hojulien02@gmail.com>
2025-07-30 13:25:57 +0200

    Ajustement de la longueur de certains champs dans les migrations User et Suggestion

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('email')->unique();
+            $table->string('username', 20)->unique();
+            $table->string('email', 60)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('avatar')->default('default.png');
